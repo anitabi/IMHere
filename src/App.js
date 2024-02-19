eb85a5ecf298c57872e46bbe58c077ac04845328
Jacky HE <ihkk.nb@gmail.com>
2024-02-19 22:06:32 +0000

    initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <span class="fs-4">I'm here 圣地巡礼对比图生成器</span>
+          </a>
+          <ul class="nav nav-pills">
+            <li><button className="btn">About</button></li>
+          </ul>
+        </header>
+      </div>
     </div>
   );
 }
