a1d45ff8e47015ec1f90ddcfe22364e47473aad5
Jacky HE <ihkk.nb@gmail.com>
2024-02-20 00:38:23 +0000

    change page title

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ function App() {
       <div className="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
           <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <span class="fs-4">I'm Here! 圣地巡礼对比图生成器</span>
+            <span class="fs-4">I'm Here!</span>
           </a>
           <ul class="nav nav-pills">
             <li><button className="btn">About</button></li>
