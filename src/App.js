dc4ffd4cfe038030a41a8d675305ac8097cfe434
Jacky HE <ihkk.nb@gmail.com>
2024-02-20 21:27:43 +0000

    update title link

--- src/App.js
+++ src/App.js
@@ -195,7 +195,7 @@ function App() {
 
       <div className="container">
         <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <a href="." class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
             <span class="fs-4">I'm Here! 圣地巡礼对比图生成器</span>
           </a>
           {/* <ul class="nav nav-pills">
