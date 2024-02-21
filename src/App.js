b08e8dd756a29b0129b4dcbbfd4b21324e865240
Jacky HE <ihkk.nb@gmail.com>
2024-02-21 18:20:19 +0000

    update header style

--- src/App.js
+++ src/App.js
@@ -207,8 +207,8 @@ function App() {
 
 
       <div className="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a href="." class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <header class="d-flex flex-wrap justify-content-center py-3 border-bottom">
+          <a href="." class="d-flex align-items-center me-md-auto text-dark text-decoration-none">
             <span class="fs-4">I'm Here! 圣地巡礼对比图生成器</span>
           </a>
           {/* <ul class="nav nav-pills">
